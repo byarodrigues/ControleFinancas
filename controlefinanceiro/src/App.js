26058ca01eac0ef610ac5f759dc8f7d336d640dc
byarodrigues <beatrizrodrigues2_@hotmail.com>
2025-04-01 13:49:41 -0300

    01-04

--- controlefinanceiro/src/App.js
+++ controlefinanceiro/src/App.js
@@ -1,17 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWord from './components/HelloWord';
 
-function App() {
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Creating a finnancial controll.
-        </p>
-      </header>
-    </div>
-  );
+function sum(a, b) {
+  return a + b
 }
+
+const url = 'https://via.placeholder.com/150'
+
+return (
+  <div className="App">
+    <h2>Alterando o JSK</h2>
+    <p>Olá, {newName}</p>
+    <p>Soma: {sum(1,2)}</p>
+    <img src={url} alt='Minha imagem'/>
+    <HelloWord/>
+  </div>
+);
 
 export default App;
--- controlefinanceiro/src/App.test.js
+++ controlefinanceiro/src/App.test.js
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-//import HelloWord from './components/HelloWord';
 
 test('renders learn react link', () => {
   render(<App />);
